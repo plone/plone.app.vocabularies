ea652d45f8e45eb02b93f6a696fce483ef63040e
Hanno Schlichting <hanno@hannosch.eu>
2007-03-03 12:41:58 +0000

    Added UserFriendlyTypesVocabulary.
    svn path=/plone.app.vocabularies/trunk/; revision=13408

--- plone/app/vocabularies/types.py
+++ plone/app/vocabularies/types.py
@@ -49,3 +49,20 @@ class PortalTypesVocabulary(object):
         return SimpleVocabulary.fromItems(items)
 
 PortalTypesVocabularyFactory = PortalTypesVocabulary()
+
+
+class UserFriendlyTypesVocabulary(object):
+    """Vocabulary factory for user friendly portal types.
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        context = getattr(context, 'context', context)
+        ptool = getToolByName(context, 'plone_utils')
+        ttool = getToolByName(context, 'portal_types')
+        items = [ (ttool[t].Title(), t)
+                  for t in ptool.getUserFriendlyTypes() ]
+        items.sort()
+        return SimpleVocabulary.fromItems(items)
+
+UserFriendlyTypesVocabularyFactory = UserFriendlyTypesVocabulary()
