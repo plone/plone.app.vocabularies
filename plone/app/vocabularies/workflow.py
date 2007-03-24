294151e387c80cc0a1c72ded95e3006b9066d358
Martin Aspeli <optilude@gmail.com>
2007-03-24 21:39:42 +0000

    We need to not have duplicate titles or ids for workflow titles
    svn path=/plone.app.vocabularies/trunk/; revision=13806

--- plone/app/vocabularies/workflow.py
+++ plone/app/vocabularies/workflow.py
@@ -41,18 +41,22 @@ class WorkflowTransitionsVocabulary(object):
     
     def __call__(self, context):
         wtool = getUtility(IConfigurableWorkflowTool)
-        transitions = []
-        dup_list = {}
+        transitions = {}
+        
         for wf in wtool.objectValues():
             transition_folder = getattr(wf, 'transitions', None)
+            wf_name = wf.title or wf.id
             if transition_folder is not None:
                 for transition in transition_folder.objectValues():
-                    key = '%s:%s' % (transition.id, transition.title,)
-                    if not dup_list.has_key(key):
-                        transitions.append(transition)
-                    dup_list[key] = True
-        items = [(s.title, s.getId()) for s in transitions]
-        items.sort()
-        return SimpleVocabulary.fromItems(items)
+                    transition_title = transition.actbox_name
+                    transitions.setdefault(transition.id, []).append(dict(title=transition_title, 
+                                                                        wf_name=wf_name))
+        items = []
+        for transition_id, info in transitions.items():
+            titles = set([i['title'] for i in info])
+            item_title = ' // '.join(sorted(titles))
+            items.append(("%s [%s]" % (item_title, transition_id,), transition_id),)
+                
+        return SimpleVocabulary.fromItems(sorted(items))
 
 WorkflowTransitionsVocabularyFactory = WorkflowTransitionsVocabulary()
