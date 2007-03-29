bba65018cc4d1c741d4692da2fee75a4ee8088b9
Sebastien Verbois <sebastien.verbois@fundp.ac.be>
2007-03-29 13:23:03 +0000

    add theme setting for icon visibility configuration
    svn path=/plone.app.vocabularies/trunk/; revision=13964

--- plone/app/vocabularies/skins.py
+++ plone/app/vocabularies/skins.py
@@ -4,9 +4,9 @@ from zope.interface import implements
 from zope.schema.vocabulary import SimpleVocabulary
 
 from Products.CMFCore.interfaces import ISkinsTool
+from Products.CMFPlone import PloneMessageFactory as _
 
-
-class SkinsVocabulary(object):
+class SkinsVocabularyFactory(object):
     """Vocabulary factory for skins.
     """
     implements(IVocabularyFactory)
@@ -18,4 +18,18 @@ class SkinsVocabulary(object):
         items.sort()
         return SimpleVocabulary.fromItems(items)
 
-SkinsVocabularyFactory = SkinsVocabulary()
+SkinsVocabularyFactory = SkinsVocabularyFactory()
+
+class IconVisibilityFactory(object):
+    """Vocabulary factory for icon visiblity.
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        vd = {
+            _(u"Disabled for anonymous"): 'authenticated',
+            _(u"Disabled for all"): 'disabled',
+            _(u"Enabled for all"): 'enabled',}
+        return SimpleVocabulary.fromItems(vd.items())
+
+IconVisibilityFactory = IconVisibilityFactory()
