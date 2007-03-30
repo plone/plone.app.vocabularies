2eb7ca18f56476c77c299015f2c287efb7f941f8
Sebastien Verbois <sebastien.verbois@fundp.ac.be>
2007-03-30 19:21:14 +0000

    remove icon visibility factory
    svn path=/plone.app.vocabularies/trunk/; revision=14005

--- plone/app/vocabularies/skins.py
+++ plone/app/vocabularies/skins.py
@@ -4,9 +4,8 @@ from zope.interface import implements
 from zope.schema.vocabulary import SimpleVocabulary
 
 from Products.CMFCore.interfaces import ISkinsTool
-from Products.CMFPlone import PloneMessageFactory as _
 
-class SkinsVocabularyFactory(object):
+class SkinsVocabulary(object):
     """Vocabulary factory for skins.
     """
     implements(IVocabularyFactory)
@@ -18,18 +17,4 @@ class SkinsVocabularyFactory(object):
         items.sort()
         return SimpleVocabulary.fromItems(items)
 
-SkinsVocabularyFactory = SkinsVocabularyFactory()
-
-class IconVisibilityFactory(object):
-    """Vocabulary factory for icon visiblity.
-    """
-    implements(IVocabularyFactory)
-
-    def __call__(self, context):
-        vd = {
-            _(u"Only for users who are logged in"): 'authenticated',
-            _(u"Never show icons"): 'disabled',
-            _(u"Always show icons"): 'enabled',}
-        return SimpleVocabulary.fromItems(vd.items())
-
-IconVisibilityFactory = IconVisibilityFactory()
+SkinsVocabularyFactory = SkinsVocabulary()
