b20d44a75f8277b2a85603c018076983dce7239d
Martin Aspeli <optilude@gmail.com>
2007-05-07 01:13:23 +0000

    Add a user source, so that we can use the UberSelectionWidget on users.
    svn path=/plone.app.vocabularies/trunk/; revision=14906

---
+++ plone/app/vocabularies/users.py
@@ -0,0 +1,66 @@
+import itertools
+from zope.interface import implements, classProvides
+from zope.schema.interfaces import ISource, IContextSourceBinder
+from zope.schema.vocabulary import SimpleTerm
+
+from zope.app.form.browser.interfaces import ISourceQueryView, ITerms
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+
+from Products.CMFCore.utils import getToolByName
+
+class UsersSource(object):
+    implements(ISource)
+    classProvides(IContextSourceBinder)
+
+    def __init__(self, context):
+        self.context = context
+        self.users = getToolByName(context, "acl_users")
+
+    def __contains__(self, value):
+        """Return whether the value is available in this source
+        """
+        if self.get(value) is None:
+            return False
+        return True
+
+    def search(self, query):
+        return [u['userid'] for u in self.users.searchUsers(fullname=query)]
+        
+    def get(self, value):
+        return self.users.getUserById(value, None)
+
+
+class UsersSourceQueryView(object):
+    implements(ITerms,
+               ISourceQueryView)
+
+    template = ViewPageTemplateFile('searchabletextsource.pt')
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def getTerm(self, value):
+        user = self.context.get(value)
+        token = value
+        title = value
+        if user is not None:
+            title = user.getProperty('fullname', None) or user.getUserName()
+        return SimpleTerm(value, token=token, title=title)
+
+    def getValue(self, token):
+        if token not in self.context:
+            LookupError(token)
+        return token
+
+    def render(self, name):
+        return self.template(name=name)
+
+    def results(self, name):
+        # check whether the normal search button was pressed
+        if name+".search" in self.request.form:
+            query_fieldname = name+".query"
+            if query_fieldname in self.request.form:
+                query = self.request.form[query_fieldname]
+                if query != '':
+                    return self.context.search(query)
