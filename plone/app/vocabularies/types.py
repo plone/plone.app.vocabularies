8e441367425e2c0a4a14ae5d35f29b81b8010ab6
Rocky Burt <rocky@serverzen.com>
2007-06-05 14:42:01 +0000

    Removed some vocabularies that are not supported (yet) with the Archetypes included with Plone 2.5.
    svn path=/plone.app.vocabularies/branches/plone-2.5/; revision=15210

--- plone/app/vocabularies/types.py
+++ plone/app/vocabularies/types.py
@@ -1,39 +1,6 @@
 from zope.app.schema.vocabulary import IVocabularyFactory
 from zope.interface import implements
 from zope.schema.vocabulary import SimpleVocabulary
-from Products.Archetypes.mimetype_utils import getAllowableContentTypes
-from Products.Archetypes.mimetype_utils import getAllowedContentTypes
-
-from Products.CMFCore.utils import getToolByName
-
-
-class AllowableContentTypesVocabulary(object):
-    """Vocabulary factory for allowable content types.
-    """
-    implements(IVocabularyFactory)
-
-    def __call__(self, context):
-        context = getattr(context, 'context', context)
-        items = [ (t, t) for t in getAllowableContentTypes(context)]
-        items.sort()
-        return SimpleVocabulary.fromItems(items)
-
-AllowableContentTypesVocabularyFactory = AllowableContentTypesVocabulary()
-
-
-class AllowedContentTypesVocabulary(object):
-    """Vocabulary factory for allowed content types.
-    """
-    implements(IVocabularyFactory)
-
-    def __call__(self, context):
-        context = getattr(context, 'context', context)
-        items = [ (t, t) for t in getAllowedContentTypes(context)]
-        items.sort()
-        return SimpleVocabulary.fromItems(items)
-
-AllowedContentTypesVocabularyFactory = AllowedContentTypesVocabulary()
-
 
 class PortalTypesVocabulary(object):
     """Vocabulary factory for portal types.
