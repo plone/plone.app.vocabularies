abea68469d08ab082ddc06114b97d8d736baed89
Thomas Desvenain <thomas.desvenain@gmail.com>
2010-06-21 15:55:54 +0000

    Internationalized editor label (especially for 'None' value)  [thomasdesvenain]
    svn path=/plone.app.vocabularies/trunk/; revision=37378

--- plone/app/vocabularies/editors.py
+++ plone/app/vocabularies/editors.py
@@ -1,3 +1,4 @@
+from zope.i18nmessageid import MessageFactory
 from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm
@@ -6,6 +7,7 @@ from zope.site.hooks import getSite
 
 from Products.CMFCore.utils import getToolByName
 
+_ = MessageFactory('plone')
 
 class AvailableEditorsVocabulary(object):
     """Vocabulary factory for available editors in the portal.
@@ -45,7 +47,7 @@ class AvailableEditorsVocabulary(object):
         pprop = getToolByName(site, 'portal_properties', None)
         if pprop is not None:
             editors = pprop.site_properties.available_editors
-            items = [SimpleTerm(e, e, e) for e in editors]
+            items = [SimpleTerm(e, e, _(e)) for e in editors]
         return SimpleVocabulary(items)
 
 AvailableEditorsVocabularyFactory = AvailableEditorsVocabulary()
