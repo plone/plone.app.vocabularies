bb2316067b50fffd5827b945e4ae0a00e48f9c5f
Eric Steele <EricSteele47@gmail.com>
2012-03-25 19:28:37 -0700

    Merge esteele-date-fields branch
    commit 62d7ba8cb2532e77daa672c8c51fbd081b1ee62d
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 23:31:18 2012 -0700
    
        Add 'before today' and 'after today'  operators.
    
    commit d8b26ecf6ee39fbebf334f476658ffb355b369fe
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 23:06:35 2012 -0700
    
        Add change log.
    
    commit 8062520bb9822b586587c4070e598c540556b9c5
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 21:55:25 2012 -0700
    
        Fix relative date searches so they're finding items between now and now+delta.
    
    commit cb27809f12696cab6a022f1d66eae9d39f0d47b7
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 19:05:20 2012 -0700
    
        Use "event end date" since we're doing the same with start dates.
    
    commit c22ca935fec8fd5251f522255ff3424b07442142
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 19:04:59 2012 -0700
    
        Improve language on relative date criteria.
    
    commit 47b711e20355982e5ca5635a8c8c84f65d15d095
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 19:04:27 2012 -0700
    
        Use relative date widget.
    
    commit 592dc18baee1203de6bd92b15bef1066ffd2da08
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 19:04:02 2012 -0700
    
        Add a "today" date operator.
    
    commit 9a2503d174512163e710d20814cda822c7d3cf1f
    Author: Eric Steele <EricSteele47@gmail.com>
    Date:   Sat Mar 24 19:02:52 2012 -0700
    
        Handle empty values on date fields.

--- plone/app/querystring/queryparser.py
+++ plone/app/querystring/queryparser.py
@@ -118,26 +118,56 @@ def _currentUser(context, row):
 
 def _lessThanRelativeDate(context, row):
     # INFO: Values is the number of days
-    values = int(row.values)
+    try:
+        values = int(row.values)
+    except ValueError:
+        values = 0
     now = DateTime()
-    my_date = now + values
-    my_date = my_date.earliestTime()
+    end_date = now + values
+    end_date = end_date.latestTime()
     row = Row(index=row.index,
               operator=row.operator,
-              values=my_date)
-    return _lessThan(context, row)
+              values=(now, end_date))
+    return _between(context, row)
 
 
 def _moreThanRelativeDate(context, row):
     # INFO: Values is the number of days
-    values = int(row.values)
+    try:
+        values = int(row.values)
+    except ValueError:
+        values = 0
     now = DateTime()
-    my_date = now + values
-    my_date = my_date.latestTime()
+    start_date = now - values
+    start_date = start_date.earliestTime()
     row = Row(index=row.index,
               operator=row.operator,
-              values=my_date)
+              values=(start_date, now))
+    return _between(context, row)
+
+
+def _today(context, row):
+    now = DateTime()
+    start_date = now.earliestTime()
+    end_date = now.latestTime()
+    row = Row(index=row.index,
+              operator=row.operator,
+              values=(start_date, end_date))
+    return _between(context, row)
+
+
+def _afterToday(context, row):
+    row = Row(index=row.index,
+              operator=row.operator,
+              values=DateTime())
     return _largerThan(context, row)
+
+
+def _beforeToday(context, row):
+    row = Row(index=row.index,
+              operator=row.operator,
+              values=DateTime())
+    return _lessThan(context, row)
 
 
 def _path(context, row):
