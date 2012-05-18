046f64fed1e8a5f778af646ac272db5f47e79e08
David Glick <dglick@gmail.com>
2012-05-18 13:36:29 -0400

    remove hard dependency on Archetypes

--- plone/app/vocabularies/types.py
+++ plone/app/vocabularies/types.py
@@ -6,9 +6,34 @@ from zope.schema.vocabulary import SimpleTerm
 from zope.site.hooks import getSite
 from zope.i18n import translate
 
-from Products.Archetypes.mimetype_utils import getAllowableContentTypes
-from Products.Archetypes.mimetype_utils import getAllowedContentTypes
 from Products.CMFCore.utils import getToolByName
+
+
+def getAllowedContentTypes(context):
+    """ computes the list of allowed content types by subtracting the site property blacklist
+        from the list of installed types.
+    """
+    allowable_types = getAllowableContentTypes(context)
+    forbidden_types = getForbiddenContentTypes(context)
+    allowed_types = [type for type in allowable_types if type not in forbidden_types]
+    return allowed_types
+
+
+def getAllowableContentTypes(context):
+    """ retrieves the list of installed content types by querying portal transforms. """
+    portal_transforms = getToolByName(context, 'portal_transforms')
+    return portal_transforms.listAvailableTextInputs()
+
+
+def getForbiddenContentTypes(context):
+    """ Convenence method for retrevng the site property 'forbidden_contenttypes'."""
+    portal_properties = getToolByName(context, 'portal_properties', None)
+    if portal_properties is not None:
+        site_properties = getattr(portal_properties, 'site_properties', None)
+        if site_properties is not None:
+            if site_properties.hasProperty('forbidden_contenttypes'):
+                return list(site_properties.getProperty('forbidden_contenttypes'))
+    return []
 
 
 class AllowableContentTypesVocabulary(object):
