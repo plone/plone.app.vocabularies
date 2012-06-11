c9c5225b1c8a47f3098b1ac1cd7bfe1b723471a0
Nathan Van Gheem <vangheem@gmail.com>
2012-06-11 02:28:48 -0500

    add vocab for feed types

---
+++ plone/app/vocabularies/syndication.py
@@ -0,0 +1,28 @@
+from zope.i18nmessageid import MessageFactory
+from zope.interface import implements
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+from Products.CMFPlone.interfaces.syndication import ISiteSyndicationSettings
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+_ = MessageFactory('plone')
+
+
+class SyndicationFeedTypesVocabulary(object):
+    """
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ISiteSyndicationSettings)
+        items = []
+        for _type in settings.allowed_feed_types:
+            name, title = _type.split('|')
+            items.append(SimpleTerm(name, name, title))
+        return SimpleVocabulary(items)
+
+SyndicationFeedTypesVocabularyFactory = SyndicationFeedTypesVocabulary()
