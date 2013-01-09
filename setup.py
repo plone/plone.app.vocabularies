992b80377c0e1d00ff6b0ee02a905fe14308302f
Rok Garbas <rok@garbas.si>
2013-01-09 20:21:35 +0100

    flake8 on setup.py

--- setup.py
+++ setup.py
@@ -2,36 +2,39 @@ from setuptools import setup, find_packages
 
 version = '2.1.9.dev0'
 
-setup(name='plone.app.vocabularies',
-      version=version,
-      description="A collection of generally useful vocabularies.",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-        ],
-      keywords='Plone Zope formlib vocabularies',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.app.vocabularies',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      extras_require=dict(
+setup(
+    name='plone.app.vocabularies',
+    version=version,
+    description="A collection of generally useful vocabularies.",
+    long_description='%s\n%s' % (
+        open("README.rst").read(),
+        open("CHANGES.rst").read()
+    ),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords='Plone Zope formlib vocabularies',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.app.vocabularies',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
         test=[
             'zope.configuration',
             'zope.testing',
             'Products.PloneTestCase',
         ]
-      ),
-      install_requires=[
+    ),
+    install_requires=[
         'setuptools',
         'zope.browser',
         'zope.component',
@@ -44,5 +47,5 @@ setup(name='plone.app.vocabularies',
         'Acquisition',
         'Products.CMFCore',
         'Zope2',
-      ],
-      )
+    ],
+)
