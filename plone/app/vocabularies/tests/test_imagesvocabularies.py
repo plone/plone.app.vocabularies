9279f4116f9d57cce3d150c3a5da35cf7d36b71f
Benoît Suttor <bsuttor@imio.be>
2016-03-29 18:12:25 +0200

    Add test for ImagesScales vocabulary

---
+++ plone/app/vocabularies/tests/test_imagesvocabularies.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from plone.app.vocabularies.testing import PAVocabularies_INTEGRATION_TESTING
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+import unittest
+
+
+class ImagesTest(unittest.TestCase):
+    layer = PAVocabularies_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_images_scales_vocabulary(self):
+        images_scales_voc = getUtility(
+            IVocabularyFactory,
+            'plone.app.vocabularies.ImagesScales'
+        )
+        scales_list = [item.value for item in images_scales_voc(self.portal)]
+        self.assertTrue('thumb' in scales_list)
+        self.assertTrue('large' in scales_list)
+        self.assertTrue('preview' in scales_list)
