bd19bf5d77d084c1c4dc16d2bba7bca598401a74
Benoît Suttor <bsuttor@imio.be>
2016-03-31 16:54:55 +0200

    Translate image scale for vocabulary

--- plone/app/vocabularies/images.py
+++ plone/app/vocabularies/images.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from plone.app.imaging.utils import getAllowedSizes
+from zope.i18nmessageid import MessageFactory
+from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
-from zope.interface import provider
+
+_ = MessageFactory('plone')
 
 
 @provider(IVocabularyFactory)
@@ -12,7 +15,10 @@ def ScalesVocabulary(context):
     """
     terms = []
     for scale, (width, height) in getAllowedSizes().iteritems():
-        terms.append(SimpleTerm(scale, scale, "{0:s} {1:d}x{2:d}".format (
-            scale, width, height)))
+        translated = _(
+            'imagescale_{0:s}'.format(scale),
+            default='{0:s} ${{width}}x${{height}}'.format(scale),
+            mapping={'width': str(width), 'height': str(height)})
+        terms.append(SimpleTerm(scale, scale, translated))
 
     return SimpleVocabulary(terms)
