b5f4c8c18e8adebca0c9c27c585255a3018e7284
Benoît Suttor <bsuttor@users.noreply.github.com>
2016-04-04 13:57:03 +0200

    Add images scales vocabulary to master
    Add ImagesScales vocabulary:
    * test for ImagesScales vocabulary
    * doc new vocabulary in Readme
    * new plone.app.imaging dependency
    * translate image scale for vocabulary

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     install_requires=[
         'Acquisition',
         'plone.app.querystring',
+        'plone.app.imaging',
         'Products.CMFCore',
         'pytz',
         'setuptools',
