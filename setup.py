2a1c073e652ec512584e5ab1a1783441aca400ce
Benoît Suttor <bsuttor@users.noreply.github.com>
2016-04-04 14:32:59 +0200

    Add images scales vocabulary
    * Add ImagesScales vocabulary
    
    * Add test for ImagesScales vocabulary
    
    * Update changelog
    
    * Fix, add number for format for python 2.6 compatibility
    
    * Add new vocabulary into Readme doc
    
    * Add plone.app.imaging dependency
    
    * Translate image scale for vocabulary
    
    * Add ImagesScales vocabulary
    
    Add test for ImagesScales vocabulary
    
    Update changelog
    
    Fix, add number for format for python 2.6 compatibility
    
    Add new vocabulary into Readme doc
    
    Add plone.app.imaging dependency
    
    Translate image scale for vocabulary

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'Acquisition',
         'plone.app.querystring',
+        'plone.app.imaging',
         'Products.CMFCore',
         'pytz',
         'setuptools',
