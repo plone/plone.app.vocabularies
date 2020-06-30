37c837babf6e35b2d08d55dab02d41573bfc039a
Vincent Fretin <vincent.fretin@gmail.com>
2020-06-30 11:42:19 +0200

    add a test for metadatafields

--- plone/app/vocabularies/metadatafields.py
+++ plone/app/vocabularies/metadatafields.py
@@ -32,6 +32,33 @@ _FIELD_LABEL = {
 
 @implementer(IVocabularyFactory)
 class MetaDataFieldsVocabulary(object):
+    """Vocabulary factory for metadata fields
+
+      >>> from zope.component import queryUtility
+      >>> from plone.app.vocabularies.tests.base import DummyCatalog
+      >>> from plone.app.vocabularies.tests.base import DummyTool
+      >>> from plone.app.vocabularies.tests.base import create_context
+
+      >>> context = create_context()
+
+      >>> catalog = DummyCatalog(())
+      >>> catalog.schema = lambda: ['ModificationDate', 'review_state', 'SomethingNew']
+      >>> context.portal_catalog = catalog
+
+      >>> name = 'plone.app.vocabularies.MetadataFields'
+      >>> util = queryUtility(IVocabularyFactory, name)
+
+      >>> fields = util(context)
+      >>> fields
+      <zope.schema.vocabulary.SimpleVocabulary object at ...>
+
+      >>> len(fields.by_token)
+      3
+
+      >>> modification_date = fields.by_token['ModificationDate']
+      >>> modification_date.title, modification_date.token, modification_date.value
+      (u'Last modified', 'ModificationDate', 'ModificationDate')
+    """
 
     def __call__(self, context):
         cat = getToolByName(context, 'portal_catalog')
