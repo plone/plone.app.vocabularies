7150a7f445a5f61899a64c1b8198017c42e142bd
Jens W. Klein <jk@kleinundpartner.at>
2022-08-17 19:40:08 +0200

    cleanup dependencies and metadata, bump to version 5.0

--- setup.py
+++ setup.py
@@ -1,32 +1,26 @@
-# -*- coding: utf-8 -*-
 from setuptools import find_packages
 from setuptools import setup
 
 
-version = '4.3.1.dev0'
+version = '5.0.0.dev0'
 
 setup(
     name='plone.app.vocabularies',
     version=version,
     description='Collection of generally useful vocabularies for Plone.',
     long_description='{0}\n{1}'.format(
-        open('README.rst').read(),
-        open('CHANGES.rst').read()
+        open('README.rst').read(), open('CHANGES.rst').read()
     ),
     classifiers=[
         'Development Status :: 6 - Mature',
         'Environment :: Web Environment',
         'Framework :: Plone',
         'Framework :: Plone :: Core',
-        'Framework :: Plone :: 5.2',
         'Framework :: Plone :: 6.0',
-        'Framework :: Zope2',
-        'Framework :: Zope :: 4',
+        'Framework :: Zope :: 5',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -41,20 +35,10 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Acquisition',
         'plone.app.querystring',
-        'Products.CMFCore',
+        'plone.base',
         'pytz',
         'setuptools',
-        'six',
-        'zope.browser',
-        'zope.component',
-        'zope.i18n',
-        'zope.i18nmessageid',
-        'zope.interface',
-        'zope.schema',
-        'zope.site',
-        'Zope2',
     ],
     extras_require=dict(
         test=[
